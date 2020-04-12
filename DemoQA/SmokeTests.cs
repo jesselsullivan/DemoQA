14c730d748e65ddd45910126f9499bb099145af9
Jesse <jesselsullivan@gmail.com>
2020-04-12 15:18:09 -0400

    Just two basic layout / super simple smoke tests so I have an initial direction and make sure it's all working

---
+++ DemoQA/SmokeTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using FluentAssertions;
+
+namespace DemoQA
+{
+    
+    [TestClass]
+    [TestCategory("Smoke Tests")]
+    public class SmokeTests
+    {
+        [TestMethod]
+        public void UserCanVisitHome()
+        {
+            string targetUrl = "https://demoqa.com/";
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(targetUrl);
+            var resultUrl = driver.Url;
+            resultUrl.Should().BeEquivalentTo(targetUrl);
+            driver.Close();
+        }
+
+        [TestMethod]
+        public void UserCanVisitInteractions()
+        {
+            string targetUrl = "https://demoqa.com/category/interactions/";
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(targetUrl);
+            var resultUrl = driver.Url;
+            resultUrl.Should().BeEquivalentTo(targetUrl);
+            driver.Close();
+        }
+    }
+}
