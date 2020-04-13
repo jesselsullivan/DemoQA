bdcd414c26a8fa7da1fbccb6788a766aa6fdc1df
Jesse <jesselsullivan@gmail.com>
2020-04-12 22:56:18 -0400

    Create files / not implemented methods for interaction tests

---
+++ DemoQA/InteractionsTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using FluentAssertions;
+
+namespace DemoQA
+{
+    [TestClass]
+    [TestCategory("Interactions Tests")]
+    public class InteractionsTests
+    {
+        [TestMethod]
+        public void UserCanSortItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void UserCanSelectItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void UserCanResizeItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void UserCanDropItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void UserCanDragItems()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DemoQA/WidgetsTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using FluentAssertions;
+
+namespace DemoQA
+{
+    [TestClass]
+    [TestCategory("Widgets Tests")]
+    public class WidgetsTests
+    {
+
+    }
+}
